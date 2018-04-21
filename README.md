7f5be561ebb4187b99b7ef5b24064a23f20018a2
Berkay Cagir <4342669+berkaycagir@users.noreply.github.com>
2018-04-21 19:28:27 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # eksi2yuz
-Twitter bot that posts titles from ekşisözlük which has more than 200 entries in the "gündem"
+
+[eksi2yuz](https://twitter.com/eksi2yuz) is a Twitter bot that posts titles from ekşisözlük which has more than 200 entries in the "gündem". It is written with Python in one night, so excuse its mistakes.
+
+## Dependencies
+
+* a recent version of Python 2 with [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/), [requests](https://github.com/requests/requests), [tweepy](https://github.com/tweepy/tweepy)
+* a Twitter account (__obviously__)
+* a scheduled command daemon
+
+## Installation
+
+1. Install all the dependencies listed.
+2. Enter the values required in the script, which can be obtained from https://developer.twitter.com
+3. Configure your scheduled command daemon to run `eksi.py` with the desired interval.
+4. Done!
+
+## Data persistence
+
+Python's [pickle](https://docs.python.org/2/library/pickle.html) module is being used for data persistence; and quite frankly I think that's enough for this.
+
+## Contribution
+
+Pull requests are welcomed.
+
+## License
+
+You can do whatever you want under the MIT license. See the `LICENSE` for more information.
---
+++ eksi.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import requests, tweepy, logging, json, pickle
+from bs4 import BeautifulSoup
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+logging.basicConfig(filename = 'eksi.log', level = logging.INFO, format = '%(asctime)s %(message)s')
+
+def get_data(page):
+    r = requests.get("https://eksisozluk.com/basliklar/gundem?p=" + page, headers = {"X-Requested-With": "XMLHttpRequest"})
+    soup = BeautifulSoup(r.text)
+
+    data = '{"current_page": "' + page + '",'
+
+    if(page == "1"):
+        data += '"page_count": "2",'
+    else:
+        data += '"page_count": "' + soup.find("div", {"class": "pager"})["data-currentpage"] + '",'
+
+    data += '"populer_title_models": ['
+
+    o = soup.find("ul", {"class": "topic-list partial"}).find_all("a")
+
+    for i in xrange(len(o)):
+        title = o[i].text
+        link = o[i]["href"]
+        entry_count = o[i].find("small").text
+        title = title.replace(' ' + entry_count, '')
+        data += '{"title": "' + title + '", "link": "' + link + '", "entry_count": "' + entry_count + '"}'
+        if i != (len(o) - 1):
+            data += ","
+    data += "]}"
+
+    return data
+
+def get_popular(page):
+    text = ""
+    try:
+        text = get_data(str(page))
+    except Exception as e:
+        logging.info('Error with getting the popular titles')
+    else:
+        logging.info('Pulled the popular titles from API')
+    return json.loads(text)
+
+def load_list():
+    try:
+        iof = open("eksi.db", "rb")
+    except Exception as e:
+        logging.info('Error with opening the database')
+    else:
+        logging.info('Opened the database for reading')
+    input_list = pickle.load(iof)
+    iof.close()
+    return input_list
+
+def save_list(input_list):
+    try:
+        iof = open("eksi.db", "wb")
+    except Exception as e:
+        logging.info('Error with opening the database')
+    else:
+        logging.info('Opened the database for writing')
+    pickle.dump(input_list, iof, 2)
+    iof.close()
+
+def main():
+    input_list = load_list()
+
+    titles = []
+
+    i = 1
+    popular = get_popular(i)
+
+    while popular["populer_title_models"] != []:
+        titles.extend([(k['title'].encode("utf-8"), k['link'].encode("utf-8"), k['entry_count']) for k in popular['populer_title_models']])
+        i += 1
+        popular = get_popular(i)
+
+    for j in xrange(len(titles)):
+        title, link, count = titles[j]
+        if title not in input_list:
+            try:
+                count = int(count)
+            except ValueError:
+                if count.endswith('b'):
+                    count = int(float(count[:-1].replace(',','.')) * 1000)
+            if count >= 200:
+                input_list.append(title)
+                to_be_posted = title + ": https://eksisozluk.com" + link
+                auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+                auth.set_access_token(access_token, access_token_secret)
+                api = tweepy.API(auth)
+                try:
+                        api.update_status(to_be_posted)
+                except tweepy.TweepError as e:
+                        logging.info('Can not tweet, reason: ' + str(e.message[0]['code']))
+                else:
+                        logging.info('Tweeted')
+    titles = dict((x, z) for x, y, z in titles)
+
+    input_list = [x for x in input_list if x in titles and ((titles.get(x).isdecimal() and int(titles.get(x)) >= 150) or titles.get(x).endswith("b"))]
+
+    save_list(input_list)
+
+main()
